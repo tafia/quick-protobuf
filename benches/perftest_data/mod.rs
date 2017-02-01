34a039acdf503c138c9882581dba7b3b7dfeb470
Johann Tuffe <tafia973@gmail.com>
2017-02-01 11:20:21 +0800

    regenerate messages

--- benches/perftest_data/mod.rs
+++ benches/perftest_data/mod.rs
@@ -3,8 +3,9 @@
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 
-use std::io::{Read, Write};
-use quick_protobuf::{MessageRead, MessageWrite, Reader, Writer, Result};
+use std::io::{Write};
+use std::borrow::Cow;
+use quick_protobuf::{MessageWrite, Reader, Writer, Result};
 use quick_protobuf::sizeofs::*;
 
 #[derive(Debug, Default, PartialEq, Clone)]
@@ -12,13 +13,13 @@ pub struct Test1 {
     pub value: Option<i32>,
 }
 
-impl MessageRead for Test1 {
-    fn from_reader<R: Read>(mut r: &mut Reader<R>) -> Result<Self> {
+impl Test1 {
+    pub fn from_reader(r: &mut Reader, bytes: &[u8]) -> Result<Self> {
         let mut msg = Self::default();
         while !r.is_eof() {
-            match r.next_tag() {
-                Ok(8) => msg.value = Some(r.read_int32()?),
-                Ok(t) => { r.read_unknown(t)?; }
+            match r.next_tag(bytes) {
+                Ok(8) => msg.value = Some(r.read_int32(bytes)?),
+                Ok(t) => { r.read_unknown(bytes, t)?; }
                 Err(e) => return Err(e),
             }
         }
@@ -42,13 +43,13 @@ pub struct TestRepeatedBool {
     pub values: Vec<bool>,
 }
 
-impl MessageRead for TestRepeatedBool {
-    fn from_reader<R: Read>(mut r: &mut Reader<R>) -> Result<Self> {
+impl TestRepeatedBool {
+    pub fn from_reader(r: &mut Reader, bytes: &[u8]) -> Result<Self> {
         let mut msg = Self::default();
         while !r.is_eof() {
-            match r.next_tag() {
-                Ok(8) => msg.values.push(r.read_bool()?),
-                Ok(t) => { r.read_unknown(t)?; }
+            match r.next_tag(bytes) {
+                Ok(8) => msg.values.push(r.read_bool(bytes)?),
+                Ok(t) => { r.read_unknown(bytes, t)?; }
                 Err(e) => return Err(e),
             }
         }
@@ -72,13 +73,13 @@ pub struct TestRepeatedPackedInt32 {
     pub values: Vec<i32>,
 }
 
-impl MessageRead for TestRepeatedPackedInt32 {
-    fn from_reader<R: Read>(mut r: &mut Reader<R>) -> Result<Self> {
+impl TestRepeatedPackedInt32 {
+    pub fn from_reader(r: &mut Reader, bytes: &[u8]) -> Result<Self> {
         let mut msg = Self::default();
         while !r.is_eof() {
-            match r.next_tag() {
-                Ok(10) => msg.values = r.read_packed_repeated_field(|r| r.read_int32())?,
-                Ok(t) => { r.read_unknown(t)?; }
+            match r.next_tag(bytes) {
+                Ok(10) => msg.values = r.read_packed(bytes, |r, bytes| r.read_int32(bytes))?,
+                Ok(t) => { r.read_unknown(bytes, t)?; }
                 Err(e) => return Err(e),
             }
         }
@@ -104,15 +105,15 @@ pub struct TestRepeatedMessages {
     pub messages3: Vec<TestRepeatedMessages>,
 }
 
-impl MessageRead for TestRepeatedMessages {
-    fn from_reader<R: Read>(mut r: &mut Reader<R>) -> Result<Self> {
+impl TestRepeatedMessages {
+    pub fn from_reader(r: &mut Reader, bytes: &[u8]) -> Result<Self> {
         let mut msg = Self::default();
         while !r.is_eof() {
-            match r.next_tag() {
-                Ok(10) => msg.messages1.push(r.read_message()?),
-                Ok(18) => msg.messages2.push(r.read_message()?),
-                Ok(26) => msg.messages3.push(r.read_message()?),
-                Ok(t) => { r.read_unknown(t)?; }
+            match r.next_tag(bytes) {
+                Ok(10) => msg.messages1.push(r.read_message(bytes, TestRepeatedMessages::from_reader)?),
+                Ok(18) => msg.messages2.push(r.read_message(bytes, TestRepeatedMessages::from_reader)?),
+                Ok(26) => msg.messages3.push(r.read_message(bytes, TestRepeatedMessages::from_reader)?),
+                Ok(t) => { r.read_unknown(bytes, t)?; }
                 Err(e) => return Err(e),
             }
         }
@@ -142,15 +143,15 @@ pub struct TestOptionalMessages {
     pub message3: Option<Box<TestOptionalMessages>>,
 }
 
-impl MessageRead for TestOptionalMessages {
-    fn from_reader<R: Read>(mut r: &mut Reader<R>) -> Result<Self> {
+impl TestOptionalMessages {
+    pub fn from_reader(r: &mut Reader, bytes: &[u8]) -> Result<Self> {
         let mut msg = Self::default();
         while !r.is_eof() {
-            match r.next_tag() {
-                Ok(10) => msg.message1 = Some(Box::new(r.read_message()?)),
-                Ok(18) => msg.message2 = Some(Box::new(r.read_message()?)),
-                Ok(26) => msg.message3 = Some(Box::new(r.read_message()?)),
-                Ok(t) => { r.read_unknown(t)?; }
+            match r.next_tag(bytes) {
+                Ok(10) => msg.message1 = Some(Box::new(r.read_message(bytes, TestOptionalMessages::from_reader)?)),
+                Ok(18) => msg.message2 = Some(Box::new(r.read_message(bytes, TestOptionalMessages::from_reader)?)),
+                Ok(26) => msg.message3 = Some(Box::new(r.read_message(bytes, TestOptionalMessages::from_reader)?)),
+                Ok(t) => { r.read_unknown(bytes, t)?; }
                 Err(e) => return Err(e),
             }
         }
@@ -174,21 +175,21 @@ impl MessageWrite for TestOptionalMessages {
 }
 
 #[derive(Debug, Default, PartialEq, Clone)]
-pub struct TestStrings {
-    pub s1: Option<String>,
-    pub s2: Option<String>,
-    pub s3: Option<String>,
+pub struct TestStrings<'a> {
+    pub s1: Option<Cow<'a, str>>,
+    pub s2: Option<Cow<'a, str>>,
+    pub s3: Option<Cow<'a, str>>,
 }
 
-impl MessageRead for TestStrings {
-    fn from_reader<R: Read>(mut r: &mut Reader<R>) -> Result<Self> {
+impl<'a> TestStrings<'a> {
+    pub fn from_reader(r: &mut Reader, bytes: &'a [u8]) -> Result<Self> {
         let mut msg = Self::default();
         while !r.is_eof() {
-            match r.next_tag() {
-                Ok(10) => msg.s1 = Some(r.read_string()?),
-                Ok(18) => msg.s2 = Some(r.read_string()?),
-                Ok(26) => msg.s3 = Some(r.read_string()?),
-                Ok(t) => { r.read_unknown(t)?; }
+            match r.next_tag(bytes) {
+                Ok(10) => msg.s1 = Some(Cow::Borrowed(r.read_string(bytes)?)),
+                Ok(18) => msg.s2 = Some(Cow::Borrowed(r.read_string(bytes)?)),
+                Ok(26) => msg.s3 = Some(Cow::Borrowed(r.read_string(bytes)?)),
+                Ok(t) => { r.read_unknown(bytes, t)?; }
                 Err(e) => return Err(e),
             }
         }
@@ -196,7 +197,7 @@ impl MessageRead for TestStrings {
     }
 }
 
-impl MessageWrite for TestStrings {
+impl<'a> MessageWrite for TestStrings<'a> {
     fn get_size(&self) -> usize {
         self.s1.as_ref().map_or(0, |m| 1 + sizeof_var_length(m.len()))
         + self.s2.as_ref().map_or(0, |m| 1 + sizeof_var_length(m.len()))
@@ -212,17 +213,17 @@ impl MessageWrite for TestStrings {
 }
 
 #[derive(Debug, Default, PartialEq, Clone)]
-pub struct TestBytes {
-    pub b1: Option<Vec<u8>>,
+pub struct TestBytes<'a> {
+    pub b1: Option<Cow<'a, [u8]>>,
 }
 
-impl MessageRead for TestBytes {
-    fn from_reader<R: Read>(mut r: &mut Reader<R>) -> Result<Self> {
+impl<'a> TestBytes<'a> {
+    pub fn from_reader(r: &mut Reader, bytes: &'a [u8]) -> Result<Self> {
         let mut msg = Self::default();
         while !r.is_eof() {
-            match r.next_tag() {
-                Ok(10) => msg.b1 = Some(r.read_bytes()?),
-                Ok(t) => { r.read_unknown(t)?; }
+            match r.next_tag(bytes) {
+                Ok(10) => msg.b1 = Some(Cow::Borrowed(r.read_bytes(bytes)?)),
+                Ok(t) => { r.read_unknown(bytes, t)?; }
                 Err(e) => return Err(e),
             }
         }
@@ -230,7 +231,7 @@ impl MessageRead for TestBytes {
     }
 }
 
-impl MessageWrite for TestBytes {
+impl<'a> MessageWrite for TestBytes<'a> {
     fn get_size(&self) -> usize {
         self.b1.as_ref().map_or(0, |m| 1 + sizeof_var_length(m.len()))
     }
@@ -242,31 +243,31 @@ impl MessageWrite for TestBytes {
 }
 
 #[derive(Debug, Default, PartialEq, Clone)]
-pub struct PerftestData {
+pub struct PerftestData<'a> {
     pub test1: Vec<Test1>,
     pub test_repeated_bool: Vec<TestRepeatedBool>,
     pub test_repeated_messages: Vec<TestRepeatedMessages>,
     pub test_optional_messages: Vec<TestOptionalMessages>,
-    pub test_strings: Vec<TestStrings>,
+    pub test_strings: Vec<TestStrings<'a>>,
     pub test_repeated_packed_int32: Vec<TestRepeatedPackedInt32>,
-    pub test_small_bytearrays: Vec<TestBytes>,
-    pub test_large_bytearrays: Vec<TestBytes>,
+    pub test_small_bytearrays: Vec<TestBytes<'a>>,
+    pub test_large_bytearrays: Vec<TestBytes<'a>>,
 }
 
-impl MessageRead for PerftestData {
-    fn from_reader<R: Read>(mut r: &mut Reader<R>) -> Result<Self> {
+impl<'a> PerftestData<'a> {
+    pub fn from_reader(r: &mut Reader, bytes: &'a [u8]) -> Result<Self> {
         let mut msg = Self::default();
         while !r.is_eof() {
-            match r.next_tag() {
-                Ok(10) => msg.test1.push(r.read_message()?),
-                Ok(18) => msg.test_repeated_bool.push(r.read_message()?),
-                Ok(26) => msg.test_repeated_messages.push(r.read_message()?),
-                Ok(34) => msg.test_optional_messages.push(r.read_message()?),
-                Ok(42) => msg.test_strings.push(r.read_message()?),
-                Ok(50) => msg.test_repeated_packed_int32.push(r.read_message()?),
-                Ok(58) => msg.test_small_bytearrays.push(r.read_message()?),
-                Ok(66) => msg.test_large_bytearrays.push(r.read_message()?),
-                Ok(t) => { r.read_unknown(t)?; }
+            match r.next_tag(bytes) {
+                Ok(10) => msg.test1.push(r.read_message(bytes, Test1::from_reader)?),
+                Ok(18) => msg.test_repeated_bool.push(r.read_message(bytes, TestRepeatedBool::from_reader)?),
+                Ok(26) => msg.test_repeated_messages.push(r.read_message(bytes, TestRepeatedMessages::from_reader)?),
+                Ok(34) => msg.test_optional_messages.push(r.read_message(bytes, TestOptionalMessages::from_reader)?),
+                Ok(42) => msg.test_strings.push(r.read_message(bytes, TestStrings::from_reader)?),
+                Ok(50) => msg.test_repeated_packed_int32.push(r.read_message(bytes, TestRepeatedPackedInt32::from_reader)?),
+                Ok(58) => msg.test_small_bytearrays.push(r.read_message(bytes, TestBytes::from_reader)?),
+                Ok(66) => msg.test_large_bytearrays.push(r.read_message(bytes, TestBytes::from_reader)?),
+                Ok(t) => { r.read_unknown(bytes, t)?; }
                 Err(e) => return Err(e),
             }
         }
@@ -274,7 +275,7 @@ impl MessageRead for PerftestData {
     }
 }
 
-impl MessageWrite for PerftestData {
+impl<'a> MessageWrite for PerftestData<'a> {
     fn get_size(&self) -> usize {
         self.test1.iter().map(|s| 1 + sizeof_var_length(s.get_size())).sum::<usize>()
         + self.test_repeated_bool.iter().map(|s| 1 + sizeof_var_length(s.get_size())).sum::<usize>()
