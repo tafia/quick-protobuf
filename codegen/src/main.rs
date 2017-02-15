bd26e7f1bd8a0cb2eb90ea9e7e5e436d5470807f
Joonas Koivunen <joonas.koivunen@gmail.com>
2017-02-15 20:55:44 +0200

    add output command line option

--- codegen/src/main.rs
+++ codegen/src/main.rs
@@ -16,11 +16,12 @@ use types::FileDescriptor;
 
 fn main() {
     let matches = App::new("pb-rs").version(crate_version!())
-        .arg(Arg::with_name("INPUT").required(true).index(1).help("The .proto file used to generate quick-protobuf code").validator(|x| ends_with_proto(x)))
+        .arg(Arg::with_name("OUTPUT").required(false).long("output").short("o").help("Generated file name, defaults to INPUT with 'rs' extension").validator(|x| extension_matches(x, "rs")))
+        .arg(Arg::with_name("INPUT").required(true).index(1).help("The .proto file used to generate quick-protobuf code").validator(|x| extension_matches(x, "proto")))
         .get_matches();
 
     let in_file: PathBuf = matches.value_of("INPUT").map(|a| a.to_string().into()).unwrap();
-    let out_file = in_file.with_extension("rs");
+    let out_file: PathBuf = matches.value_of("OUTPUT").map(|a| a.to_string().into()).unwrap_or(in_file.with_extension("rs"));
 
     FileDescriptor::write_proto(&in_file, &out_file)
         .expect(&format!("Could not convert {} into {}", 
@@ -28,10 +29,10 @@ fn main() {
 
 }
 
-fn ends_with_proto<P: AsRef<Path>>(path: P) -> Result<(), String> {
+fn extension_matches<P: AsRef<Path>>(path: P, expected: &str) -> Result<(), String> {
     match path.as_ref().extension() {
-        Some(x) if x == "proto" => Ok(()),
-        Some(x) => Err(format!("Expected path with extension 'proto', not: '{}'", x.to_string_lossy())),
-        None => Err(format!("Expected path with extension 'proto'")),
+        Some(x) if x == expected => Ok(()),
+        Some(x) => Err(format!("Expected path with extension '{}', not: '{}'", expected, x.to_string_lossy())),
+        None => Err(format!("Expected path with extension '{}'", expected)),
     }
 }
