6b3e1e5acf54ebc15c2983b408501c876939e7ff
Joonas Koivunen <joonas.koivunen@gmail.com>
2017-02-16 19:15:22 +0200

    generate: fix logic errors, better formatting

--- tests/rust_protobuf/generate.sh
+++ tests/rust_protobuf/generate.sh
@@ -6,24 +6,40 @@ failures=0
 have_failures=""
 
 declare -A must_fail
-must_fail["../tests/rust_protobuf/v2/test_group_pb.rs"]=""
+must_fail["../tests/rust_protobuf/v2/test_group_pb.proto"]="expected failure (empty read)"
 
 declare -A outs
+
+expecting_failure() {
+	[ "${must_fail["$1"]+_}" ]
+}
+
+expecting_success() {
+	! expecting_failure "$1"
+}
+
+success_msg() {
+	if expecting_failure "$1"; then
+		echo "${must_fail["$1"]}"
+	else
+		echo "ok"
+	fi
+}
 
 for f in ../tests/rust_protobuf/v2/*.proto; do
 	ret=0
 	out="$(cargo run --quiet -- "$f" 2>&1)" || ret=$?
 
-	if [ "${must_fail["$f"]+_}" ] && [ "$ret" -eq 0 ]; then
+	if expecting_failure "$f" && [ "$ret" -eq 0 ]; then
 		outs["$f"]="$out"
 		have_failures="true"
 		echo "$f: unexpected success"
-	elif [ "$ret" -ne 0 ]; then
+	elif expecting_success "$f" && [ "$ret" -ne 0 ]; then
 		have_failures="true"
 		outs["$f"]="$out"
-		echo "$f: unexpected failure $?"
+		echo "$f: unexpected failure $ret"
 	else
-		echo "$f: ok"
+		echo "$f: $(success_msg "$f")"
 	fi
 done
 
@@ -31,16 +47,16 @@ for f in ../tests/rust_protobuf/v3/*.proto; do
 	ret=0
 	out="$(cargo run --quiet -- "$f" 2>&1)" || ret=$?
 
-	if [ "${must_fail["$f"]+_}" ] && [ "$ret" -eq 0 ]; then
+	if expecting_failure "$f" && [ "$ret" -eq 0 ]; then
 		have_failures="true"
 		outs["$f"]="$out"
 		echo "$f: unexpected success"
-	elif [ "$ret" -ne 0 ]; then
+	elif expecting_success "$f" && [ "$ret" -ne 0 ]; then
 		have_failures="true"
 		outs["$f"]="$out"
-		echo "$f: unexpected failure $?"
+		echo "$f: unexpected failure $ret"
 	else
-		echo "$f: ok"
+		echo "$f: $(success_msg "$f")"
 	fi
 done
 
@@ -48,9 +64,9 @@ if [ "$have_failures" ]; then
 	echo
 	echo "There were code generation failures:" >&2
 	for f in "${!outs[@]}"; do
+		echo
 		echo "$f:"
 		echo "${outs["$f"]}"
-		echo
 	done
 	exit 1
 fi
