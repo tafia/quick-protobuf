ceabfd72db526bf493ad11433b0a625a993cdd19
Joonas Koivunen <joonas.koivunen@gmail.com>
2017-02-16 18:58:51 +0200

    beef up the test harness, exposing existing failures

--- codegen/run_test.sh
+++ codegen/run_test.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 cargo run ../examples/codegen/data_types_import.proto
 cargo run ../examples/codegen/data_types.proto
 cd ..
 cargo run --example codegen_example
 
 cd tests/rust_protobuf
-sh generate.sh
+./generate.sh
--- tests/rust_protobuf/generate.sh
+++ tests/rust_protobuf/generate.sh
@@ -1,9 +1,56 @@
+#!/bin/bash
+
 cd ../../codegen
 
+failures=0
+have_failures=""
+
+declare -A must_fail
+must_fail["../tests/rust_protobuf/v2/test_group_pb.rs"]=""
+
+declare -A outs
+
 for f in ../tests/rust_protobuf/v2/*.proto; do
-    cargo run "$f"
+	ret=0
+	out="$(cargo run --quiet -- "$f" 2>&1)" || ret=$?
+
+	if [ "${must_fail["$f"]+_}" ] && [ "$ret" -eq 0 ]; then
+		outs["$f"]="$out"
+		have_failures="true"
+		echo "$f: unexpected success"
+	elif [ "$ret" -ne 0 ]; then
+		have_failures="true"
+		outs["$f"]="$out"
+		echo "$f: unexpected failure $?"
+	else
+		echo "$f: ok"
+	fi
 done
 
 for f in ../tests/rust_protobuf/v3/*.proto; do
-    cargo run "$f"
+	ret=0
+	out="$(cargo run --quiet -- "$f" 2>&1)" || ret=$?
+
+	if [ "${must_fail["$f"]+_}" ] && [ "$ret" -eq 0 ]; then
+		have_failures="true"
+		outs["$f"]="$out"
+		echo "$f: unexpected success"
+	elif [ "$ret" -ne 0 ]; then
+		have_failures="true"
+		outs["$f"]="$out"
+		echo "$f: unexpected failure $?"
+	else
+		echo "$f: ok"
+	fi
 done
+
+if [ "$have_failures" ]; then
+	echo
+	echo "There were code generation failures:" >&2
+	for f in "${!outs[@]}"; do
+		echo "$f:"
+		echo "${outs["$f"]}"
+		echo
+	done
+	exit 1
+fi
