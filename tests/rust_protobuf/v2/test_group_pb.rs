62818ae00c44b44aa194e109e1a61145a859b2a0
Joonas Koivunen <joonas.koivunen@gmail.com>
2017-02-17 10:22:23 +0200

    remove outputs from failing codegen cases

--- tests/rust_protobuf/v2/test_group_pb.rs
+++
@@ -1,9 +0,0 @@
-//! Automatically generated rust module for 'test_group_pb.proto' file
-
-#![allow(non_snake_case)]
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(unknown_lints)]
-#![allow(clippy)]
-#[cfg_attr(rustfmt, rustfmt_skip)]
-
