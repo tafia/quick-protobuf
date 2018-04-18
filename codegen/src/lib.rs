adb4a2e3464f924e1ed69b7ead48379bafd9fed8
jackalcooper <jackalcooper@gmail.com>
2018-04-18 14:36:08 +0800

    add lib.rs

---
+++ codegen/src/lib.rs
@@ -0,0 +1,11 @@
+#[macro_use]
+extern crate clap;
+extern crate failure;
+#[macro_use]
+extern crate failure_derive;
+#[macro_use]
+extern crate nom;
+pub mod parser;
+pub mod types;
+pub mod errors;
+pub mod keywords;
