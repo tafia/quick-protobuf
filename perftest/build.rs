90860acaadf94b78281c1e37191427d07844ec7d
James Prince <34577138+zutils@users.noreply.github.com>
2019-02-02 12:27:40 -0500

    Added RPC test to perftest
    Yes - I know it's not performance based. Boilerplate code is there if necessary.

--- perftest/build.rs
+++ perftest/build.rs
@@ -2,9 +2,27 @@ extern crate pb_rs;
 extern crate prost_build;
 extern crate protobuf_codegen_pure;
 
-use pb_rs::types::{Config, FileDescriptor};
+use pb_rs::types::{RpcService, Config, FileDescriptor};
 use std::path::{Path, PathBuf};
 use std::{env, fs};
+use std::io::Write;
+
+fn generate_rpc_add_1_test<W: Write + ?Sized>(rpc: &RpcService, w: &mut W) -> Result<(), pb_rs::errors::Error> {
+    /* Example:
+        trait <service> {
+            fn <func>(&self, arg: &<arg>) -> Result<<ret>, failure::Error>;
+        }
+    */
+
+    writeln!(w, "\npub trait {SERVICE} {{", SERVICE = rpc.service_name);
+    for func in rpc.functions.iter() {
+        writeln!(w, "   fn {FUNC}(&self, arg: &{ARG}) -> std::result::Result<{RET}, failure::Error>;", 
+            FUNC = func.name, ARG = func.arg, RET = func.ret);
+    }
+    writeln!(w, "}}\n");
+
+    Ok(())
+}
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
@@ -30,7 +48,8 @@ fn main() {
         no_output: false,
         error_cycle: false,
         headers: false,
-        custom_struct_derive: vec![]
+        custom_struct_derive: vec![],
+        custom_rpc_generator: Box::new(|rpc, writer| generate_rpc_add_1_test(rpc, writer))
     };
     FileDescriptor::write_proto(&config).unwrap();
 
