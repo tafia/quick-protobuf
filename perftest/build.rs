39ad744779e0c51a710d791e961d13ba83523648
Julian Popescu <jpopesculian@gmail.com>
2020-03-21 19:05:43 +0100

    fix perftest to work with no_std

--- perftest/build.rs
+++ perftest/build.rs
@@ -62,6 +62,7 @@ fn main() {
         custom_rpc_generator: Box::new(|rpc, writer| generate_rpc_test(rpc, writer)),
         custom_includes: Vec::new(),
         owned: false,
+        hashbrown: false,
         nostd: false,
     };
     FileDescriptor::write_proto(&config).unwrap();
