41e45a4005381c4e2b308fa6bb4d78614f7728ac
Johann Tuffe <tafia973@gmail.com>
2020-05-18 21:23:13 +0800

    deprecated failure for quick-protobuf

--- quick-protobuf/src/errors.rs
+++ quick-protobuf/src/errors.rs
@@ -1,47 +1,27 @@
 //! A module to handle all errors via failure crate
 
-#[cfg(feature = "std")]
-use std::io;
-
-/// An error enum which derives `Fail`
-#[derive(Debug, failure_derive::Fail)]
+/// An error enum
+#[derive(Debug)]
 pub enum Error {
     /// Io error
     #[cfg(feature = "std")]
-    #[fail(display = "{}", _0)]
-    Io(#[cause] io::Error),
-
+    Io(std::io::Error),
     /// Utf8 Error
-    #[fail(display = "{}", _0)]
     Utf8(::core::str::Utf8Error),
-
     /// Deprecated feature (in protocol buffer specification)
-    #[fail(display = "Feature '{}' has been deprecated", _0)]
     Deprecated(&'static str),
-
     /// Unknown wire type
-    #[fail(display = "Unknown wire type '{}', must be less than 6", _0)]
     UnknownWireType(u8),
-
     /// Varint decoding error
-    #[fail(display = "Cannot decode varint")]
     Varint,
-
     /// Error while parsing protocol buffer message
     #[cfg(feature = "std")]
-    #[fail(display = "Error while parsing message: {}", _0)]
     Message(String),
-
     /// Unexpected map tag
-    #[fail(display = "Unexpected map tag: '{}', expecting 1 or 2", _0)]
     Map(u8),
-
     /// Out of data when reading from or writing to a byte buffer
-    #[fail(display = "Unexpected end of buffer")]
     UnexpectedEndOfBuffer,
-
     /// The supplied output buffer is not large enough to serialize the message
-    #[fail(display = "Output buffer too small")]
     OutputBufferTooSmall,
 }
 
@@ -49,20 +29,19 @@ pub enum Error {
 pub type Result<T> = ::core::result::Result<T, Error>;
 
 #[cfg(feature = "std")]
-impl Into<io::Error> for Error {
+impl Into<std::io::Error> for Error {
     fn into(self) -> ::std::io::Error {
-        use failure::Fail;
         match self {
             Error::Io(x) => x,
-            Error::Utf8(x) => io::Error::new(io::ErrorKind::InvalidData, x),
-            x => io::Error::new(io::ErrorKind::Other, x.compat()),
+            Error::Utf8(x) => std::io::Error::new(std::io::ErrorKind::InvalidData, x),
+            x => std::io::Error::new(std::io::ErrorKind::Other, x),
         }
     }
 }
 
 #[cfg(feature = "std")]
-impl From<io::Error> for Error {
-    fn from(e: io::Error) -> Error {
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Error {
         Error::Io(e)
     }
 }
@@ -70,5 +49,34 @@ impl From<io::Error> for Error {
 impl From<::core::str::Utf8Error> for Error {
     fn from(e: ::core::str::Utf8Error) -> Error {
         Error::Utf8(e)
+    }
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Error::Io(e) => Some(e),
+            Error::Utf8(e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+impl core::fmt::Display for Error {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        match self {
+            #[cfg(feature = "std")]
+            Error::Io(e) => write!(f, "{}", e),
+            Error::Utf8(e) => write!(f, "{}", e),
+            Error::Deprecated(feature) => write!(f, "Feature '{}' has been deprecated", feature),
+            Error::UnknownWireType(e) => write!(f, "Unknown wire type '{}', must be less than 6", e),
+            Error::Varint => write!(f, "Cannot decode varint"),
+            #[cfg(feature = "std")]
+            Error::Message(msg) => write!(f, "Error while parsing message: {}", msg),
+            Error::Map(tag) => write!(f, "Unexpected map tag: '{}', expecting 1 or 2", tag),
+            Error::UnexpectedEndOfBuffer => write!(f, "Unexpected end of buffer"),
+            Error::OutputBufferTooSmall => write!(f, "Output buffer too small"),
+        }
     }
 }
